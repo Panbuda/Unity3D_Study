09fa542e1cd92b111cdc64a955d00c6b0189537e
Panbuda <2279502146@qq.com>
2019-12-03 23:52:47 +0800

    commit RPGTutorial_Brackeys/ Project

---
+++ UnityWorkbench/RPGTutorial_Brackeys/Assets/Scripts/Interactable.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+	public float radius = 3f;
+	public Transform interactionTransform;
+
+
+	bool isFocused = false;
+	Transform player;
+	bool hasInteracted = false;
+
+	private void OnDrawGizmosSelected()
+	{
+		Gizmos.color = Color.red;
+		if(interactionTransform != null)
+			Gizmos.DrawWireSphere(interactionTransform.position, radius);
+	}
+
+	public virtual void Interact()
+	{
+		Debug.Log("Interact with " + transform.name);
+	}
+
+	private void Update()
+	{
+		if (isFocused && !hasInteracted)
+		{
+			float distance = Vector3.Distance(interactionTransform.position, player.position);
+			if(distance <= radius)
+			{
+				Interact();
+				hasInteracted = true;
+			}
+		}
+	}
+
+	public void OnFocused(Transform player)
+	{
+		isFocused = true;
+		this.player = player;
+		//the next line will result in the Interact function be triggered some times, without it, we need to defocus to retrigger the Interact function
+		//hasInteracted = false;
+	}
+
+	public void OnDeFocused()
+	{
+		isFocused = false;
+		player = null;
+		hasInteracted = false;
+	}
+}
---
+++ UnityWorkbench/RPGTutorial_Brackeys/Assets/Study Note.md
@@ -0,0 +1,12 @@
+# Study Note
+
+## 01
+
+* Nav Mesh Agent.
+* Mathf.Clamp()[CameraController]
+
+## 02
+
+* OnDrawGizmosSelected()[Interactable]
+* Elegant achievement of interaction.
+
